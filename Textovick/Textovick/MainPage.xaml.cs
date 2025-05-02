d3a4623bc80f7697b15086117116bb0f6500dcaa
Егор <@DESKTOP-5MITQ43>
2025-05-02 16:15:25 +0500

    Добавьте файлы проекта.

---
+++ Textovick/Textovick/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Textovick
+{
+    public partial class MainPage : ContentPage
+    {
+        string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            UpdateFileList();
+        }
+
+        async void Save(object sender, EventArgs e)
+        {
+            string filename = fileNameEntry.Text;
+            if (String.IsNullOrEmpty(filename)) return;
+            if (File.Exists(Path.Combine(folderPath, filename)))
+            {
+                bool isRewrited = await DisplayAlert("Подтверждение", "Файл уже существует, перезаписать его?", "Да", "Нет");
+                if (isRewrited == false) return;
+            }
+
+            File.WriteAllText(Path.Combine(folderPath, filename), textEditor.Text);
+
+            UpdateFileList();
+        }
+
+        void FileSelect(object sender, SelectedItemChangedEventArgs args)
+        {
+            if (args.SelectedItem == null) return;
+            string filename = (string)args.SelectedItem;
+            textEditor.Text = File.ReadAllText(Path.Combine(folderPath, (string)args.SelectedItem));
+            fileNameEntry.Text = filename;
+            filesList.SelectedItem = null;
+        }
+
+        void Delete(object sender, EventArgs args)
+        {
+            string filename = (string)((MenuItem)sender).BindingContext;
+            File.Delete(Path.Combine(folderPath, filename));
+            UpdateFileList();
+        }
+
+        void UpdateFileList()
+        {
+            filesList.ItemsSource = Directory.GetFiles(folderPath).Select(f => Path.GetFileName(f));
+            filesList.SelectedItem = null;
+        }
+    }
+}
